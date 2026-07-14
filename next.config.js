2e8e96dd0a5f93b9f620066621a9c76c06014e4c
Soft7Technology <aakanksha@soft7.in>
2026-07-14 13:16:53 +0530

    edit

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
       },
       {
         source: '/auth/:path*',
-        destination: 'http://127.0.0.1:5000/auth/:path*',
+        destination: 'http://127.0.0.1:8000/auth/:path*',
       },
     ];
   },
